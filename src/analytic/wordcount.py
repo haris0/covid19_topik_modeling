1950ca9e20d77038b08fa17138a6fc1ba4e4ea35
haris0 <hari.surrisyad@gmail.com>
2020-04-11 04:57:09 +0700

    showing wordclound and wordcount

---
+++ src/analytic/wordcount.py
@@ -0,0 +1,46 @@
+
+import nltk
+from nltk.probability import FreqDist
+import pandas as pd
+import matplotlib.pyplot as plt
+import streamlit as st
+from wordcloud import WordCloud
+
+def marge_all_text(df):
+  string = []
+  for t in df.tweet:
+    string.append(t)
+  string = pd.Series(string).str.cat(sep=' ')
+  return string
+
+def get_top15(df):
+  string = marge_all_text(df)
+  tokens = nltk.word_tokenize(string)
+  fdist = FreqDist()
+  for word in tokens:
+      fdist[word.lower()] += 1
+  print(fdist)
+
+  fdist_top15 = fdist.most_common(15)
+  df_top15 = pd.DataFrame(fdist_top15, columns=['words', 'count'])
+
+  fig, ax = plt.subplots(figsize=(8, 5))
+
+  # Plot horizontal bar graph
+  df_top15.sort_values(by='count').plot.barh(x='words',
+                        y='count',
+                        ax=ax,
+                        color="blue")
+
+  ax.set_title("Daftar 15 kata yang paling sering muncul")
+  st.pyplot()
+  return fdist_top15
+
+def show_wordclound(df):
+  string = marge_all_text(df)
+  print(string)
+  wordcloud = WordCloud(background_color="white").generate(string)
+  plt.figure()
+  plt.imshow(wordcloud, interpolation="bilinear")
+  plt.axis("off")
+  st.pyplot()
--- src/view/topik_modeling.py
+++ src/view/topik_modeling.py
@@ -3,12 +3,14 @@ import time
 import pandas as pd
 from tweet_fun.scraper import load_tweet
 from tweet_fun import cleaning as cln
+import analytic.wordcount as wc
+import numpy as np
 
+# mengetahui topik bembahasan apa saja yang sedang dibicarakan terkait covid19*
 def topik_modeling():
-  st.title('Pemodelan Topik Covid19')
-  st.write('Pada halaman ini akan dilakuakn pemodelan topik dari 1000 data tweet terbaru '+
-          'berbahahsa indonesia yang mengandung kata covid19, hal ini dilakuakn untuk '+
-          '**mengetahui topik bembahasan apa saja yang sedang dibicarakan terkait covid19**')
+  st.title('Analisis Tweet Covid19')
+  st.write("""Pada halaman ini akan dilakuakn analsis terhadap 1000 data tweet terbaru
+          berbahahsa indonesia yang mengandung kata covid19, mencakup **Wordcount**, dan **Pemodelan Topik**""")
   st.text('')
   keyword = 'covid19'
   if st.button("Start Analysis"):
@@ -18,11 +20,25 @@ def topik_modeling():
     st.subheader('Raw Data')
     st.dataframe(df)
 
-
     with st.spinner('Cleaning Data...'):
       df = cln.clean_part1(df)
       df = cln.clean_part2(df)
-      df = cln.clean_part3(df,keyword)
+      df = cln.clean_part3(df,'covid')
     st.subheader('Clean Data')
     st.dataframe(df)
+
+    st.title('Wordcount')
+    st.write("""Akan dilakukan penghitungan kata, untuk mengetahui kata yang paling sering muncul
+            dari 1000 data tweet, hal ini dilakuakn untuk memperkirakan keyword dalam topik pembahsan
+            data covid""") 
+
+    st.subheader("Wordcloud :")
+    wc.show_wordclound(df)
+
+    st.subheader("Top 15 Word :")
+    top15 = wc.get_top15(df)
+    st.subheader("Dengan Rincian :")
+    for i,word in enumerate(top15):
+      st.write(str(i+1)+" **"+word[0]+"** disebut sebanyak "+str(word[1])+" kali")
+    
 
