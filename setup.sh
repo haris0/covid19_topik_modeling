69d9f8713b71c316a68053659d18fec3217eab06
haris0 <hari.surrisyad@gmail.com>
2020-04-20 11:13:58 +0700

    remove token

--- setup.sh
+++ setup.sh
@@ -9,6 +9,5 @@ echo "\
 [server]\n\
 headless = true\n\
 enableCORS=false\n\
-token = \"pk.eyJ1IjoiaGFyaXMwIiwiYSI6ImNrOTd5NXBudzAxc2ozZ3JvcDZxajd1dXQifQ.p82ei9zcSndouW9JYUlQUw\"\n\
 port = $PORT\n\
 " > ~/.streamlit/config.toml
